7d54a53d901c1ccec6f008bb004c52e853becbd5
takke <hiroaki@v7.com>
2020-06-24 13:31:04 +0900

    CpuNotificationDataDistributorTest.kt

---
+++ app/src/test/java/jp/takke/cpustats/CpuNotificationDataDistributorTest.kt
@@ -0,0 +1,318 @@
+package jp.takke.cpustats
+
+import jp.takke.cpustats.CpuNotificationDataDistributor.distributeNotificationData
+import org.hamcrest.CoreMatchers
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
+import org.robolectric.shadows.ShadowLog
+
+@RunWith(RobolectricTestRunner::class)
+@Config(constants = BuildConfig::class, sdk = [21])
+class CpuNotificationDataDistributorTest {
+    @Before
+    @Throws(Exception::class)
+    fun setUp() {
+        ShadowLog.stream = System.out
+    }
+
+    @Test
+    fun distributeNotificationData_2icons() {
+
+        // core1
+        run {
+            val cpuUsages = intArrayOf(30, 30)
+            val data = distributeNotificationData(cpuUsages, C.CORE_DISTRIBUTION_MODE_2ICONS)
+            Assert.assertThat(data.size, CoreMatchers.`is`(1))
+            Assert.assertThat(data[0].coreNoStart, CoreMatchers.`is`(1))
+            Assert.assertThat(data[0].coreNoEnd, CoreMatchers.`is`(1))
+            Assert.assertThat(data[0].cpuUsages, CoreMatchers.`is`(intArrayOf(30, 30)))
+        }
+
+        // core2
+        run {
+            val cpuUsages = intArrayOf(60, 30, 90)
+            val data = distributeNotificationData(cpuUsages, C.CORE_DISTRIBUTION_MODE_2ICONS)
+            Assert.assertThat(data.size, CoreMatchers.`is`(1))
+            Assert.assertThat(data[0].coreNoStart, CoreMatchers.`is`(1))
+            Assert.assertThat(data[0].coreNoEnd, CoreMatchers.`is`(2))
+            Assert.assertThat(data[0].cpuUsages, CoreMatchers.`is`(intArrayOf(60, 30, 90)))
+        }
+
+        // core3
+        run {
+            val cpuUsages = intArrayOf(50, 30, 90, 30)
+            val data = distributeNotificationData(cpuUsages, C.CORE_DISTRIBUTION_MODE_2ICONS)
+            Assert.assertThat(data.size, CoreMatchers.`is`(1))
+            Assert.assertThat(data[0].coreNoStart, CoreMatchers.`is`(1))
+            Assert.assertThat(data[0].coreNoEnd, CoreMatchers.`is`(3))
+            Assert.assertThat(data[0].cpuUsages, CoreMatchers.`is`(intArrayOf(50, 30, 90, 30)))
+        }
+
+        // core4
+        run {
+            val cpuUsages = intArrayOf(40, 30, 90, 30, 10)
+            val data = distributeNotificationData(cpuUsages, C.CORE_DISTRIBUTION_MODE_2ICONS)
+            Assert.assertThat(data.size, CoreMatchers.`is`(1))
+            Assert.assertThat(data[0].coreNoStart, CoreMatchers.`is`(1))
+            Assert.assertThat(data[0].coreNoEnd, CoreMatchers.`is`(4))
+            Assert.assertThat(data[0].cpuUsages, CoreMatchers.`is`(intArrayOf(40, 30, 90, 30, 10)))
+        }
+
+        // core5
+        run {
+            val cpuUsages = intArrayOf(40, 30, 90, 30, 10, 40)
+            val data = distributeNotificationData(cpuUsages, C.CORE_DISTRIBUTION_MODE_2ICONS)
+            Assert.assertThat(data.size, CoreMatchers.`is`(2))
+            Assert.assertThat(data[0].coreNoStart, CoreMatchers.`is`(1))
+            Assert.assertThat(data[0].coreNoEnd, CoreMatchers.`is`(4))
+            Assert.assertThat(data[0].cpuUsages, CoreMatchers.`is`(intArrayOf(40, 30, 90, 30, 10)))
+            Assert.assertThat(data[1].coreNoStart, CoreMatchers.`is`(5))
+            Assert.assertThat(data[1].coreNoEnd, CoreMatchers.`is`(5))
+            Assert.assertThat(data[1].cpuUsages, CoreMatchers.`is`(intArrayOf(40, 40)))
+        }
+
+        // core6
+        run {
+            val cpuUsages = intArrayOf(40, 10, 20, 30, 70, 50, 60)
+            val data = distributeNotificationData(cpuUsages, C.CORE_DISTRIBUTION_MODE_2ICONS)
+            Assert.assertThat(data.size, CoreMatchers.`is`(2))
+            Assert.assertThat(data[0].coreNoStart, CoreMatchers.`is`(1))
+            Assert.assertThat(data[0].coreNoEnd, CoreMatchers.`is`(3))
+            Assert.assertThat(data[0].cpuUsages, CoreMatchers.`is`(intArrayOf(40, 10, 20, 30)))
+            Assert.assertThat(data[1].coreNoStart, CoreMatchers.`is`(4))
+            Assert.assertThat(data[1].coreNoEnd, CoreMatchers.`is`(6))
+            Assert.assertThat(data[1].cpuUsages, CoreMatchers.`is`(intArrayOf(40, 70, 50, 60)))
+        }
+
+        // core7
+        run {
+            val cpuUsages = intArrayOf(40, 10, 20, 30, 70, 50, 60, 40)
+            val data = distributeNotificationData(cpuUsages, C.CORE_DISTRIBUTION_MODE_2ICONS)
+            Assert.assertThat(data.size, CoreMatchers.`is`(2))
+            Assert.assertThat(data[0].coreNoStart, CoreMatchers.`is`(1))
+            Assert.assertThat(data[0].coreNoEnd, CoreMatchers.`is`(4))
+            Assert.assertThat(data[0].cpuUsages, CoreMatchers.`is`(intArrayOf(40, 10, 20, 30, 70)))
+            Assert.assertThat(data[1].coreNoStart, CoreMatchers.`is`(5))
+            Assert.assertThat(data[1].coreNoEnd, CoreMatchers.`is`(7))
+            Assert.assertThat(data[1].cpuUsages, CoreMatchers.`is`(intArrayOf(40, 50, 60, 40)))
+        }
+
+        // core8
+        run {
+            val cpuUsages = intArrayOf(45, 10, 30, 50, 20, 40, 80, 70, 60)
+            val data = distributeNotificationData(cpuUsages, C.CORE_DISTRIBUTION_MODE_2ICONS)
+            Assert.assertThat(data.size, CoreMatchers.`is`(2))
+            Assert.assertThat(data[0].coreNoStart, CoreMatchers.`is`(1))
+            Assert.assertThat(data[0].coreNoEnd, CoreMatchers.`is`(4))
+            Assert.assertThat(data[0].cpuUsages, CoreMatchers.`is`(intArrayOf(45, 10, 30, 50, 20)))
+            Assert.assertThat(data[1].coreNoStart, CoreMatchers.`is`(5))
+            Assert.assertThat(data[1].coreNoEnd, CoreMatchers.`is`(8))
+            Assert.assertThat(data[1].cpuUsages, CoreMatchers.`is`(intArrayOf(45, 40, 80, 70, 60)))
+        }
+
+        // core9
+        run {
+            val cpuUsages = intArrayOf(45, 10, 30, 50, 20, 40, 80, 70, 60, 90)
+            val data = distributeNotificationData(cpuUsages, C.CORE_DISTRIBUTION_MODE_2ICONS)
+            Assert.assertThat(data.size, CoreMatchers.`is`(2))
+            Assert.assertThat(data[0].coreNoStart, CoreMatchers.`is`(1))
+            Assert.assertThat(data[0].coreNoEnd, CoreMatchers.`is`(4))
+            Assert.assertThat(data[0].cpuUsages, CoreMatchers.`is`(intArrayOf(45, 10, 30, 50, 20)))
+            Assert.assertThat(data[1].coreNoStart, CoreMatchers.`is`(5))
+            Assert.assertThat(data[1].coreNoEnd, CoreMatchers.`is`(9))
+            Assert.assertThat(data[1].cpuUsages, CoreMatchers.`is`(intArrayOf(45, 40, 80, 70, 60, 90)))
+        }
+    }
+
+    @Test
+    fun distributeNotificationData_1icon_unsorted() {
+
+        // core1
+        run {
+            val cpuUsages = intArrayOf(30, 30)
+            val data = distributeNotificationData(cpuUsages, C.CORE_DISTRIBUTION_MODE_1ICON_UNSORTED)
+            Assert.assertThat(data.size, CoreMatchers.`is`(1))
+            Assert.assertThat(data[0].coreNoStart, CoreMatchers.`is`(1))
+            Assert.assertThat(data[0].coreNoEnd, CoreMatchers.`is`(1))
+            Assert.assertThat(data[0].cpuUsages, CoreMatchers.`is`(intArrayOf(30, 30)))
+        }
+
+        // core2
+        run {
+            val cpuUsages = intArrayOf(60, 30, 90)
+            val data = distributeNotificationData(cpuUsages, C.CORE_DISTRIBUTION_MODE_1ICON_UNSORTED)
+            Assert.assertThat(data.size, CoreMatchers.`is`(1))
+            Assert.assertThat(data[0].coreNoStart, CoreMatchers.`is`(1))
+            Assert.assertThat(data[0].coreNoEnd, CoreMatchers.`is`(2))
+            Assert.assertThat(data[0].cpuUsages, CoreMatchers.`is`(intArrayOf(60, 30, 90)))
+        }
+
+        // core3
+        run {
+            val cpuUsages = intArrayOf(50, 30, 90, 30)
+            val data = distributeNotificationData(cpuUsages, C.CORE_DISTRIBUTION_MODE_1ICON_UNSORTED)
+            Assert.assertThat(data.size, CoreMatchers.`is`(1))
+            Assert.assertThat(data[0].coreNoStart, CoreMatchers.`is`(1))
+            Assert.assertThat(data[0].coreNoEnd, CoreMatchers.`is`(3))
+            Assert.assertThat(data[0].cpuUsages, CoreMatchers.`is`(intArrayOf(50, 30, 90, 30)))
+        }
+
+        // core4
+        run {
+            val cpuUsages = intArrayOf(40, 30, 90, 30, 10)
+            val data = distributeNotificationData(cpuUsages, C.CORE_DISTRIBUTION_MODE_1ICON_UNSORTED)
+            Assert.assertThat(data.size, CoreMatchers.`is`(1))
+            Assert.assertThat(data[0].coreNoStart, CoreMatchers.`is`(1))
+            Assert.assertThat(data[0].coreNoEnd, CoreMatchers.`is`(4))
+            Assert.assertThat(data[0].cpuUsages, CoreMatchers.`is`(intArrayOf(40, 30, 90, 30, 10)))
+        }
+
+        // core5
+        run {
+            val cpuUsages = intArrayOf(40, 30, 90, 30, 10, 40)
+            val data = distributeNotificationData(cpuUsages, C.CORE_DISTRIBUTION_MODE_1ICON_UNSORTED)
+            Assert.assertThat(data.size, CoreMatchers.`is`(1))
+            Assert.assertThat(data[0].coreNoStart, CoreMatchers.`is`(1))
+            Assert.assertThat(data[0].coreNoEnd, CoreMatchers.`is`(4))
+            Assert.assertThat(data[0].cpuUsages, CoreMatchers.`is`(intArrayOf(40, 30, 90, 30, 10)))
+        }
+
+        // core6
+        run {
+            val cpuUsages = intArrayOf(40, 10, 20, 30, 70, 50, 60)
+            val data = distributeNotificationData(cpuUsages, C.CORE_DISTRIBUTION_MODE_1ICON_UNSORTED)
+            Assert.assertThat(data.size, CoreMatchers.`is`(1))
+            Assert.assertThat(data[0].coreNoStart, CoreMatchers.`is`(1))
+            Assert.assertThat(data[0].coreNoEnd, CoreMatchers.`is`(4))
+            Assert.assertThat(data[0].cpuUsages, CoreMatchers.`is`(intArrayOf(40, 10, 20, 30, 70)))
+        }
+
+        // core7
+        run {
+            val cpuUsages = intArrayOf(40, 10, 20, 30, 70, 50, 60, 40)
+            val data = distributeNotificationData(cpuUsages, C.CORE_DISTRIBUTION_MODE_1ICON_UNSORTED)
+            Assert.assertThat(data.size, CoreMatchers.`is`(1))
+            Assert.assertThat(data[0].coreNoStart, CoreMatchers.`is`(1))
+            Assert.assertThat(data[0].coreNoEnd, CoreMatchers.`is`(4))
+            Assert.assertThat(data[0].cpuUsages, CoreMatchers.`is`(intArrayOf(40, 10, 20, 30, 70)))
+        }
+
+        // core8
+        run {
+            val cpuUsages = intArrayOf(45, 10, 30, 50, 20, 40, 80, 70, 60)
+            val data = distributeNotificationData(cpuUsages, C.CORE_DISTRIBUTION_MODE_1ICON_UNSORTED)
+            Assert.assertThat(data.size, CoreMatchers.`is`(1))
+            Assert.assertThat(data[0].coreNoStart, CoreMatchers.`is`(1))
+            Assert.assertThat(data[0].coreNoEnd, CoreMatchers.`is`(4))
+            Assert.assertThat(data[0].cpuUsages, CoreMatchers.`is`(intArrayOf(45, 10, 30, 50, 20)))
+        }
+
+        // core9
+        run {
+            val cpuUsages = intArrayOf(45, 10, 30, 50, 20, 40, 80, 70, 60, 90)
+            val data = distributeNotificationData(cpuUsages, C.CORE_DISTRIBUTION_MODE_1ICON_UNSORTED)
+            Assert.assertThat(data.size, CoreMatchers.`is`(1))
+            Assert.assertThat(data[0].coreNoStart, CoreMatchers.`is`(1))
+            Assert.assertThat(data[0].coreNoEnd, CoreMatchers.`is`(4))
+            Assert.assertThat(data[0].cpuUsages, CoreMatchers.`is`(intArrayOf(45, 10, 30, 50, 20)))
+        }
+    }
+
+    @Test
+    fun distributeNotificationData_1icon_sorted() {
+
+        // core1
+        run {
+            val cpuUsages = intArrayOf(30, 30)
+            val data = distributeNotificationData(cpuUsages, C.CORE_DISTRIBUTION_MODE_1ICON_SORTED)
+            Assert.assertThat(data.size, CoreMatchers.`is`(1))
+            Assert.assertThat(data[0].coreNoStart, CoreMatchers.`is`(1))
+            Assert.assertThat(data[0].coreNoEnd, CoreMatchers.`is`(1))
+            Assert.assertThat(data[0].cpuUsages, CoreMatchers.`is`(intArrayOf(30, 30)))
+        }
+
+        // core2
+        run {
+            val cpuUsages = intArrayOf(60, 30, 90)
+            val data = distributeNotificationData(cpuUsages, C.CORE_DISTRIBUTION_MODE_1ICON_SORTED)
+            Assert.assertThat(data.size, CoreMatchers.`is`(1))
+            Assert.assertThat(data[0].coreNoStart, CoreMatchers.`is`(1))
+            Assert.assertThat(data[0].coreNoEnd, CoreMatchers.`is`(2))
+            Assert.assertThat(data[0].cpuUsages, CoreMatchers.`is`(intArrayOf(60, 90, 30)))
+        }
+
+        // core3
+        run {
+            val cpuUsages = intArrayOf(50, 30, 90, 20)
+            val data = distributeNotificationData(cpuUsages, C.CORE_DISTRIBUTION_MODE_1ICON_SORTED)
+            Assert.assertThat(data.size, CoreMatchers.`is`(1))
+            Assert.assertThat(data[0].coreNoStart, CoreMatchers.`is`(1))
+            Assert.assertThat(data[0].coreNoEnd, CoreMatchers.`is`(3))
+            Assert.assertThat(data[0].cpuUsages, CoreMatchers.`is`(intArrayOf(50, 90, 30, 20)))
+        }
+
+        // core4
+        run {
+            val cpuUsages = intArrayOf(40, 20, 90, 30, 10)
+            val data = distributeNotificationData(cpuUsages, C.CORE_DISTRIBUTION_MODE_1ICON_SORTED)
+            Assert.assertThat(data.size, CoreMatchers.`is`(1))
+            Assert.assertThat(data[0].coreNoStart, CoreMatchers.`is`(1))
+            Assert.assertThat(data[0].coreNoEnd, CoreMatchers.`is`(4))
+            Assert.assertThat(data[0].cpuUsages, CoreMatchers.`is`(intArrayOf(40, 90, 30, 20, 10)))
+        }
+
+        // core5
+        run {
+            val cpuUsages = intArrayOf(40, 30, 90, 10, 5, 40)
+            val data = distributeNotificationData(cpuUsages, C.CORE_DISTRIBUTION_MODE_1ICON_SORTED)
+            Assert.assertThat(data.size, CoreMatchers.`is`(1))
+            Assert.assertThat(data[0].coreNoStart, CoreMatchers.`is`(1))
+            Assert.assertThat(data[0].coreNoEnd, CoreMatchers.`is`(4))
+            Assert.assertThat(data[0].cpuUsages, CoreMatchers.`is`(intArrayOf(40, 90, 40, 30, 10)))
+        }
+
+        // core6
+        run {
+            val cpuUsages = intArrayOf(40, 10, 20, 30, 70, 50, 60)
+            val data = distributeNotificationData(cpuUsages, C.CORE_DISTRIBUTION_MODE_1ICON_SORTED)
+            Assert.assertThat(data.size, CoreMatchers.`is`(1))
+            Assert.assertThat(data[0].coreNoStart, CoreMatchers.`is`(1))
+            Assert.assertThat(data[0].coreNoEnd, CoreMatchers.`is`(4))
+            Assert.assertThat(data[0].cpuUsages, CoreMatchers.`is`(intArrayOf(40, 70, 60, 50, 30)))
+        }
+
+        // core7
+        run {
+            val cpuUsages = intArrayOf(40, 10, 20, 30, 70, 50, 60, 40)
+            val data = distributeNotificationData(cpuUsages, C.CORE_DISTRIBUTION_MODE_1ICON_SORTED)
+            Assert.assertThat(data.size, CoreMatchers.`is`(1))
+            Assert.assertThat(data[0].coreNoStart, CoreMatchers.`is`(1))
+            Assert.assertThat(data[0].coreNoEnd, CoreMatchers.`is`(4))
+            Assert.assertThat(data[0].cpuUsages, CoreMatchers.`is`(intArrayOf(40, 70, 60, 50, 40)))
+        }
+
+        // core8
+        run {
+            val cpuUsages = intArrayOf(45, 10, 30, 50, 20, 40, 80, 70, 60)
+            val data = distributeNotificationData(cpuUsages, C.CORE_DISTRIBUTION_MODE_1ICON_SORTED)
+            Assert.assertThat(data.size, CoreMatchers.`is`(1))
+            Assert.assertThat(data[0].coreNoStart, CoreMatchers.`is`(1))
+            Assert.assertThat(data[0].coreNoEnd, CoreMatchers.`is`(4))
+            Assert.assertThat(data[0].cpuUsages, CoreMatchers.`is`(intArrayOf(45, 80, 70, 60, 50)))
+        }
+
+        // core9
+        run {
+            val cpuUsages = intArrayOf(45, 10, 30, 50, 20, 40, 80, 70, 60, 90)
+            val data = distributeNotificationData(cpuUsages, C.CORE_DISTRIBUTION_MODE_1ICON_SORTED)
+            Assert.assertThat(data.size, CoreMatchers.`is`(1))
+            Assert.assertThat(data[0].coreNoStart, CoreMatchers.`is`(1))
+            Assert.assertThat(data[0].coreNoEnd, CoreMatchers.`is`(4))
+            Assert.assertThat(data[0].cpuUsages, CoreMatchers.`is`(intArrayOf(45, 90, 80, 70, 60)))
+        }
+    }
+}
