038f991471c8d6de413f11009f3484709539acd4
takke <takke30@gmail.com>
2025-04-29 23:06:06 +0900

    reformat

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,134 +2,140 @@ import java.text.SimpleDateFormat
 import java.util.Date
 
 plugins {
-    id("com.android.application")
-    id("kotlin-android")
+  id("com.android.application")
+  id("kotlin-android")
 }
 
 kotlin {
-    jvmToolchain(11)
+  jvmToolchain(11)
 }
 
 android {
-    signingConfigs {
-        create("myConfig")
+  signingConfigs {
+    create("myConfig")
+  }
+
+  namespace = "jp.takke.cpustats"
+
+  compileSdk = rootProject.extra["compileSdkVersion"] as Int
+  buildToolsVersion = rootProject.extra["buildToolsVersion"] as String
+
+  defaultConfig {
+    applicationId = "jp.takke.cpustats"
+    targetSdk = rootProject.extra["targetSdkVersion"] as Int
+    minSdk = rootProject.extra["minSdkVersion"] as Int
+    versionCode = rootProject.extra["versionCode"] as Int
+    val versionName = rootProject.extra["versionName"] as String
+
+    val shortVersionName = versionName.replace(".", "")
+    val d = SimpleDateFormat("yyyyMMdd_HHmm").format(Date())
+    setProperty(
+      "archivesBaseName",
+      "${rootProject.extra["apkNamePrefix"]}_${shortVersionName}_${d}"
+    )
+  }
+
+  buildTypes {
+    getByName("release") {
+      isMinifyEnabled = true
+      isShrinkResources = false
+      proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+      signingConfig = signingConfigs.getByName("myConfig")
     }
-
-    namespace = "jp.takke.cpustats"
-
-    compileSdk = rootProject.extra["compileSdkVersion"] as Int
-    buildToolsVersion = rootProject.extra["buildToolsVersion"] as String
-
-    defaultConfig {
-        applicationId = "jp.takke.cpustats"
-        targetSdk = rootProject.extra["targetSdkVersion"] as Int
-        minSdk = rootProject.extra["minSdkVersion"] as Int
-        versionCode = rootProject.extra["versionCode"] as Int
-        val versionName = rootProject.extra["versionName"] as String
-
-        val shortVersionName = versionName.replace(".", "")
-        val d = SimpleDateFormat("yyyyMMdd_HHmm").format(Date())
-        setProperty("archivesBaseName", "${rootProject.extra["apkNamePrefix"]}_${shortVersionName}_${d}")
+    getByName("debug") {
+      applicationIdSuffix = ".debug"
+      versionNameSuffix = "d"
     }
+  }
 
-    buildTypes {
-        getByName("release") {
-            isMinifyEnabled = true
-            isShrinkResources = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-            signingConfig = signingConfigs.getByName("myConfig")
+  buildFeatures {
+    buildConfig = true
+    aidl = true
+  }
+
+  flavorDimensions += "edition"
+
+  productFlavors {
+    create("quad5") {
+      dimension = "edition"
+    }
+  }
+
+  val publish = tasks.register("publishAll")
+  applicationVariants.all {
+    if (buildType.name == "release") {
+      outputs.forEach { output ->
+        if (output.outputFile.name.endsWith(".apk")) {
+          @Suppress("DEPRECATION")
+          val variantName = name.capitalize()
+          val task = tasks.register("publish${variantName}Apk", Copy::class) {
+            val srcDir = packageApplicationProvider.get().outputDirectory.get().asFile
+            val srcPath = srcDir.resolve(output.outputFile.name)
+            from(srcPath)
+            into(rootProject.extra["deployTo"] as String)
+            dependsOn(assembleProvider.get())
+          }
+          publish.configure { dependsOn(task) }
         }
-        getByName("debug") {
-            applicationIdSuffix = ".debug"
-            versionNameSuffix = "d"
-        }
+      }
     }
+  }
 
-    buildFeatures {
-        buildConfig = true
-        aidl = true
-    }
+  applicationVariants.all {
+    if (buildType.name == "release") {
+      val flavorName0 = productFlavors[0].name
+      val buildTypeName = buildType.name
 
-    flavorDimensions += "edition"
-
-    productFlavors {
-        create("quad5") {
-            dimension = "edition"
-        }
-    }
-
-    val publish = tasks.register("publishAll")
-    applicationVariants.all {
-        if (buildType.name == "release") {
-            outputs.forEach { output ->
-                if (output.outputFile.name.endsWith(".apk")) {
-                    @Suppress("DEPRECATION")
-                    val variantName = name.capitalize()
-                    val task = tasks.register("publish${variantName}Apk", Copy::class) {
-                        val srcDir = packageApplicationProvider.get().outputDirectory.get().asFile
-                        val srcPath = srcDir.resolve(output.outputFile.name)
-                        from(srcPath)
-                        into(rootProject.extra["deployTo"] as String)
-                        dependsOn(assembleProvider.get())
-                    }
-                    publish.configure { dependsOn(task) }
-                }
-            }
-        }
-    }
-
-    applicationVariants.all {
-        if (buildType.name == "release") {
-            val flavorName0 = productFlavors[0].name
-            val buildTypeName = buildType.name
-
-            @Suppress("DEPRECATION")
-            val variantName = name.capitalize()
-            tasks.register("bundlePublish${variantName}", Copy::class) {
-                val aabFilename = "${base.archivesName.get()}-$flavorName0-${buildTypeName}.aab"
+      @Suppress("DEPRECATION")
+      val variantName = name.capitalize()
+      tasks.register("bundlePublish${variantName}", Copy::class) {
+        val aabFilename = "${base.archivesName.get()}-$flavorName0-${buildTypeName}.aab"
 //                val aabFilename = "${rootProject.extra["base.archivesName"]}-$flavorName0-$buildTypeName.aab"
-                val path = "${layout.buildDirectory.get()}/outputs/bundle/${variantName}/$aabFilename"
-                println("${name}: $path -> ${rootProject.extra["deployTo"]}")
+        val path = "${layout.buildDirectory.get()}/outputs/bundle/${variantName}/$aabFilename"
+        println("${name}: $path -> ${rootProject.extra["deployTo"]}")
 //                println("*** aab path = $path ($task) [$flavorName0] [$buildTypeName]")
-                from(path)
-                into(rootProject.extra["deployTo"] as String)
-                dependsOn("bundle${variantName}")
-            }
-        }
+        from(path)
+        into(rootProject.extra["deployTo"] as String)
+        dependsOn("bundle${variantName}")
+      }
     }
+  }
 
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
+  compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+  }
 }
 
 dependencies {
-    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+  implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
 
-    implementation(libs.androidx.appcompat)
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.annotation)
-    implementation(libs.androidx.constraintlayout)
-    implementation(libs.androidx.preference.ktx)
+  implementation(libs.androidx.appcompat)
+  implementation(libs.androidx.core.ktx)
+  implementation(libs.androidx.annotation)
+  implementation(libs.androidx.constraintlayout)
+  implementation(libs.androidx.preference.ktx)
 
-    testImplementation(libs.robolectric)
-    testImplementation(libs.junit)
-    testImplementation(libs.assertj.core)
+  testImplementation(libs.robolectric)
+  testImplementation(libs.junit)
+  testImplementation(libs.assertj.core)
 
-    implementation(libs.kotlin.stdlib.jdk7)
+  implementation(libs.kotlin.stdlib.jdk7)
 }
 
 // load signing settings from gradle.properties
 if (project.hasProperty("storeFile")) {
-    android.signingConfigs.getByName("myConfig").storeFile = file(project.property("storeFile") as String)
+  android.signingConfigs.getByName("myConfig").storeFile =
+    file(project.property("storeFile") as String)
 }
 if (project.hasProperty("storePassword")) {
-    android.signingConfigs.getByName("myConfig").storePassword = project.property("storePassword") as String
+  android.signingConfigs.getByName("myConfig").storePassword =
+    project.property("storePassword") as String
 }
 if (project.hasProperty("keyAlias")) {
-    android.signingConfigs.getByName("myConfig").keyAlias = project.property("keyAlias") as String
+  android.signingConfigs.getByName("myConfig").keyAlias = project.property("keyAlias") as String
 }
 if (project.hasProperty("keyPassword")) {
-    android.signingConfigs.getByName("myConfig").keyPassword = project.property("keyPassword") as String
+  android.signingConfigs.getByName("myConfig").keyPassword =
+    project.property("keyPassword") as String
 }
