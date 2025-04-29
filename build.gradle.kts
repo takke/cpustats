612a8a5ecdcff9d21a3e03d514ecff41f342ec1b
takke <takke30@gmail.com>
2025-04-29 22:12:49 +0900

    build.gradle.kts WIP

---
+++ build.gradle.kts
@@ -0,0 +1,37 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+buildscript {
+    repositories {
+        mavenCentral()
+        google()
+    }
+
+    dependencies {
+        classpath("com.android.tools.build:gradle:${libs.versions.agp.get()}")
+
+        // Kotlin
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${libs.versions.kotlin.get()}")
+    }
+}
+
+allprojects {
+    repositories {
+        mavenCentral()
+        google()
+    }
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.layout.buildDirectory)
+}
+
+ext {
+    set("apkNamePrefix", "CpuStats")
+    set("versionName", "2.2.2")
+    set("versionCode", 26)
+
+    set("compileSdkVersion", 34)
+    set("buildToolsVersion", "35.0.0")
+    set("targetSdkVersion", 34)
+    set("minSdkVersion", 23)
+}
